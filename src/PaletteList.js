14719e807df2198950e14c5c327aad5eaf2a9a04
Roger Rigoni <rogerrigoni.a@gmail.com>
2022-10-16 18:44:06 -0300

    Finish Styling MiniPalette

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -7,7 +7,7 @@ const styles = {
 
     root: {
         backgroundColor: "blue",
-        height: "100%",
+        height: "100vh",
         display: "flex",
         alignItems: "flex-start",
         justifyContent: "center"
