fdeb8b7f489e569e75c7bc71107bc5d45d5f34a3
Roger Rigoni <rogerrigoni.a@gmail.com>
2022-10-16 17:42:10 -0300

    Add Palette List and Links

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import { generatePalette } from "./colorHelpers";
 import Palette from "./Palette";
+import PaletteList from "./PaletteList";
 import seedColors from "./seedColors";
 
 class App extends Component {
   findPalette(id) {
-   return seedColors.find(function (palette) {
+    return seedColors.find(function (palette) {
       return palette.id === id
     })
   }
   render() {
     return (
       <Switch>
-        <Route exact path="/" render={() => <h1>PALETTES</h1>} />
+        <Route exact path="/" render={() => <PaletteList palettes={seedColors} />} />
         <Route
           exact
           path="/palette/:id"
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Select from '@material-ui/core/Select';
 import Slider, { Range } from 'rc-slider';
 import { IconButton, MenuItem } from '@material-ui/core';
@@ -27,7 +28,7 @@ class Navbar extends Component {
         return (
             <header className='Navbar'>
                 <div className='logo'>
-                    <a href='#'>reactcolorpicker</a>
+                    <Link to="/">reactcolorpicker</Link>
                 </div>
                 <div className='slider-container'>
                     <span>Level: {level}</span>
